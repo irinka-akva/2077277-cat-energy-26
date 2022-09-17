830784c594d34f9047277bb059ed0b6d7d0baf02
Irina <irinka-akva@ya.ru>
2022-09-18 01:05:35 +0300

    Исправляет работу раскрывающегося меню

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,7 @@ let nav = document.querySelector('.navigation');
 let navToggle = document.querySelector('.navigation__toggle');
 
 nav.classList.remove('navigation--no-js');
+nav.classList.add('navigation--close');
 
 navToggle.addEventListener('click', function () {
   if (nav.classList.contains('navigation--close')) {
